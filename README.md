dae082e14f6fa2149996826206eaef5a9497d918
dahlo <martin.dahlo@scilifelab.uu.se>
2017-06-28 23:00:42 +0200

    Final commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # blocket_watcher
 Watches blocket for new ads and alerts when there is something new.
+
+# Requirements
+Uses Mailgun.org to send emails. Get a domain and account there. Or rewrite the email sending to use gmail or whatever service you want.
+
+# Installation
+Rename watcher.conf.dist to watcher.conf and modify the file with your settings.
+
+# Usage
+Add one instance per url you want to watch to your crontab, maybe run it every 10 min or so. Try not to run multiple instances at the same time, as they will probably overwrite the `history.db` file and mess stuff up for each other.
+
+python blocket_watcher.py <url to watch>
+
+Ex.
+
+```bash
+$ python blocket_watcher.py https://www.blocket.se/uppsala?q=skor&cg=0&w=1&st=s&c=&ca=10&is=1&l=0&md=th
+```
---
+++ watcher.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+
+from ConfigParser import SafeConfigParser
+import os
+import sys
+import json
+import urllib
+from IPython.core.debugger import Tracer
+from datetime import datetime
+import requests
+
+from email.mime.text import MIMEText
+from subprocess import Popen, PIPE
+
+
+try:
+    from bs4 import BeautifulSoup
+except ImportError:
+    from BeautifulSoup import BeautifulSoup
+
+
+
+
+def alert_user(new_ads, config, url):
+
+	# pluralize
+	multiple = ""
+	if len(new_ads) > 1:
+		multiple = "s"
+	
+
+	# get the search string
+	search_string = url.split('q=')[1].split('&')[0].replace('+',' ')
+
+	
+
+	# send an email to 
+	msg = u"""Hello there
+There are {} new ad{} for the Blocket search '{}', {}
+
+
+""".format(len(new_ads), multiple, search_string, url)
+
+	for ad in new_ads:
+
+		# get ad info
+		price = ad.div.p.text
+		if not price:
+			price = "No price"
+
+		title = ad.div.h1.a['title']
+		link = ad.div.h1.a['href']
+		
+		msg += u"""{}
+{}
+{}
+
+""".format(title, price, link)
+
+
+	msg += "\nCheers\nThe Blocket Watcher"
+		
+		
+	# Tracer()()
+
+	requests.post(
+        "https://api.mailgun.net/v3/{}/messages".format(config.get('settings', 'mailgun_domain')),
+        auth=("api", config.get('settings', 'mailgun_key')),
+        data={"from": "Blocket Watcher <blocket.watcher@{}>".format(config.get('settings', 'mailgun_domain')),
+              "to": [config.get('settings', 'email')],
+              "subject": "Blocket Watcher: {} new ad{} for '{}'".format(len(new_ads), multiple, search_string),
+              "text": msg})
+
+
+
+
+
+
+# get working dir
+workdir = os.path.dirname(os.path.realpath(__file__))
+
+
+
+config = SafeConfigParser()
+config.read('{}/watcher.conf'.format(workdir))
+
+
+
+# get the url to watch
+usage = "python {} <url to watch>".format(sys.argv[0])
+try:
+	url = sys.argv[1]
+except:
+	url = "https://www.blocket.se/uppsala?q=yoyo+babyzen&cg=0&w=2&st=s&ca=10&is=1&l=0&md=th"
+	# sys.exit(usage)
+
+
+# try to open the history file
+try:
+	with open("{}/history.json".format(workdir), 'r') as history_file:
+		history = json.load(history_file)
+except:
+	print("Failed to open history file {}, creating a new one.".format("history.json"))
+	history = {url:{'date':'1970-01-01 00:00:00'}}
+
+
+
+# open the url to watch
+resp = urllib.urlopen(url) 
+html = resp.read()   
+page = BeautifulSoup(html, "lxml")
+
+# create date to compare to
+last_update = datetime.strptime(history[url]['date'], '%Y-%m-%d %H:%M:%S')
+
+# init
+new_ads = []
+
+# find all ads
+for ad in page.findAll('article', 'media'):
+
+	# get ad date
+	ad_date = datetime.strptime(ad.div.header.time['datetime'], '%Y-%m-%d %H:%M:%S')
+
+	# check if it's a new ad
+	if ad_date > last_update:
+
+		# the ad is new and should be reported to the user
+		new_ads.append(ad)
+
+	# Tracer()()
+
+
+
+# if there are any new ads to report
+if new_ads:
+	alert_user(new_ads, config, url)
+
+
+
+# update the history
+history[url]['date'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+# save the history file again
+# with open("{}/history.json".format(workdir), "w") as history_file:
+# 	json.dump(history, history_file)
